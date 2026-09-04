ef9698683e889d7f31531d91d8522e0e34ec4836
CharlesJr16 <charlesjrcosta12@gmail.com>
2026-09-04 00:12:45 -0300

    03/09

--- help-bunny!/player.gd
+++ help-bunny!/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -620.0
 
 # Pega a gravidade configurada nas configurações do projeto
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
